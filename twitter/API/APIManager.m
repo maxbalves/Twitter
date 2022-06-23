95f9966ebef95ea656fa0bd22bce3d1ba4aec764
Max Bagatini Alves <maxbagatini@hotmail.com>
2022-06-23 13:17:05 -0700

    Uploaded gif

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -72,7 +72,7 @@ static NSString * const baseURLString = @"https://api.twitter.com";
 // Post Composed Tweet Method
 - (void)postStatusWithText:(NSString *)text completion:(void (^)(Tweet *, NSError *))completion {
     NSString *urlString = @"1.1/statuses/update.json";
-    NSDictionary *parameters = @{@"status": text};
+    NSDictionary *parameters = @{@"status": text, @"tweet_mode": @"extended"};
     
     [self POST:urlString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nullable tweetDictionary) {
         Tweet *tweet = [[Tweet alloc]initWithDictionary:tweetDictionary];
